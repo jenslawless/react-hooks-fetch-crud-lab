fafb8284eda3f67b3cc97b2f7fa9601fd63762a9
Jensen Lawless <jensenlawless@gmail.com>
2023-05-30 14:59:51 -0600

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((prompts) => {
+        setQuestions(prompts);
+      });
+  }, []);
+
+  function handleAddNewQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion])
+  }
+
+  function handleDeleteQuestion(deletedQuestion) {
+    const updatedQuestions = questions.filter((question) => question.id !== deletedQuestion.id);
+    setQuestions(updatedQuestions)
+  }
+
+  function handleAnswerUpdate(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ correctIndex })
+    })
+      .then((r) => r.json())
+      .then((updatedQuestion) => {
+        const updatedQuestions = questions.map((q) => {
+          if (q.id === updatedQuestion.id) return updatedQuestion;
+          return q
+        })
+        setQuestions(updatedQuestions)
+      });
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm addNewQuestion={handleAddNewQuestion} /> : <QuestionList questions={questions} onQuestionDelete={handleDeleteQuestion} onAnswerUpdate={handleAnswerUpdate} />}
     </main>
   );
 }
